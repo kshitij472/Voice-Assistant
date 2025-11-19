376dfba165710931d96c002616fa25da867b3a75
kshitij472 <chouhankshitij85@gmail.com>
2025-11-19 19:16:06 +0530

    updated file

--- VOICE.py
+++ VOICE.py
@@ -5,3 +5,8 @@ load_dotenv()
 
 AGENT_ID = os.getenv("agent_9801kae42540fybr0kxyxbbwggdz")
 API_KEY = os.getenv("sk_06658ff9ee6309af11ad288f05f845a8c06dbc488d7d154f")
+
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.types import ConversationConfig
