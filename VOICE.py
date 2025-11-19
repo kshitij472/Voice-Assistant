45f5bea2165c6ed1bebdeab11d257eef9b08b770
kshitij472 <chouhankshitij85@gmail.com>
2025-11-19 19:04:16 +0530

    updated file

---
+++ VOICE.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AGENT_ID = os.getenv("agent_9801kae42540fybr0kxyxbbwggdz")
+API_KEY = os.getenv("sk_06658ff9ee6309af11ad288f05f845a8c06dbc488d7d154f")
