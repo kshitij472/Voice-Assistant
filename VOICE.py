2bccbc4ce61eeac321594090feaf892827b642af
kshitij472 <chouhankshitij85@gmail.com>
2025-11-20 13:15:29 +0530

    updated file

--- VOICE.py
+++ VOICE.py
@@ -10,3 +10,55 @@ from elevenlabs.client import ElevenLabs
 from elevenlabs.conversational_ai.conversation import Conversation
 from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
 from elevenlabs.types import ConversationConfig
+
+user_name = "Kshitij"
+schedule = "Sales Meeting with Taipy at 10:00; Gym with Sophie at 17:00"
+prompt = f"You are a helpful assistant. Your interlocutor has the following schedule: {schedule}."
+first_message = f"Hye {user_name}, how can I help you today?"
+
+conversation_override = {
+    "agent": {
+        "prompt": {
+            "prompt": prompt,
+        },
+        "first_message": first_message,
+    },
+}
+
+config = ConversationConfig(
+    conversation_config_override=conversation_override,
+    extra_body={},
+    dynamic_variables={},
+)
+
+client = ElevenLabs(api_key = "sk_06658ff9ee6309af11ad288f05f845a8c06dbc488d7d154f")
+
+conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+)
+
+def print_agent_response(response):
+    print(f"Agent: {response}")
+    
+def print_interrupted_response(original, corrected):
+    print(f"Agent interrupted, truncated response: {corrected}")
+    
+def print_user_transcript(transcript):
+    print(f"User: {transcript}")
+    
+conversation = Conversation(
+  client,
+  AGENT_ID,
+  config=config,
+  requires_auth=True,
+  audio_interface=DefaultAudioInterface(),
+  callback_agent_response=print_agent_response,
+  callback_agent_response_correction=print_interrupted_response,
+  callback_user_transcript=print_user_transcript,
+)
+
+conversation.start_session()
